9bd26c62e81cf43d932c2cfa362e54bf851ce92f
ctrangel <122327961+ctrangel@users.noreply.github.com>
2024-09-15 11:46:51 -0400

    M2

---
+++ Module2/playground-1.mongodb.js
@@ -0,0 +1,22 @@
+
+
+use('DB_uno');
+
+
+db.getCollection("Plant").insertMany([
+  { plant: "yes", name: "Rose", color: "Red" },
+  { plant: "yes", name: "Tulip", color: "Yellow" },
+  { plant: "yes", name: "Lily", color: "White" },
+  { plant: "yes", name: "Daisy", color: "White" },
+  { plant: "yes", name: "Sunflower", color: "Yellow" },
+  { plant: "yes", name: "Orchid", color: "Purple" },
+  { plant: "yes", name: "Fern", color: "Green" },
+  { plant: "yes", name: "Cactus", color: "Green" },
+  { plant: "yes", name: "Palm", color: "Green" },
+  { plant: "yes", name: "Cherry Blossom", color: "Pink" },
+  { plant: "yes", name: "Lavender", color: "Purple" },
+  { plant: "yes", name: "Bamboo", color: "Green" },
+  { plant: "yes", name: "Ivy", color: "Green" },
+  { plant: "yes", name: "Aloe Vera", color: "Green" },
+  { plant: "yes", name: "Magnolia", color: "White" },
+]);
